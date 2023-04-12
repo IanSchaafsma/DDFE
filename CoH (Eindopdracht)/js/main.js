77a3b880ff5040206fa5853a9f677e9eefbc37be
Ian <33319@ma-web.nl>
2023-04-12 11:23:59 +0200

    add card styling

--- CoH (Eindopdracht)/js/main.js
+++ CoH (Eindopdracht)/js/main.js
@@ -1,1 +1,74 @@
-console.log("ik werk");
+class getDataFromApi{
+    url = ""
+    data = null;
+
+    constructor(newURL){
+        this.url = newURL;
+    }
+
+    async getData(){
+            await fetch(this.url)
+            .then(function (response) {
+                return response.json();
+            }).then((data) => {
+                this.data = data;
+            });
+        return this.data;
+    }
+}
+
+class Header{
+    constructor(){
+
+    }
+
+    render(){
+
+    }
+}
+
+class LeftPanel{
+    constructor(){
+
+    }
+
+    render(){
+        
+    }
+}
+
+class RightPanel{
+    constructor(){
+
+    }
+
+    render(){
+        
+    }
+}
+
+class DetailCard{
+    constructor(){
+
+    }
+
+    render(){
+        
+    }
+}
+
+class Footer{
+    constructor(){
+
+    }
+
+    render(){
+        
+    }
+}
+
+class App{
+    constructor(){
+
+    }    
+}
