a04dc6c4ca95e99e15a6274caa9b60a12a1a539f
Ian Schaafsma <you@example.com>
2023-04-18 23:32:55 +0200

    add js classes

--- BacklogChooser/js/main.js
+++ BacklogChooser/js/main.js
@@ -1,4 +1,4 @@
-class GetData{
+class Api{
     data = null;
     async getData(){
         await fetch("../data/games.json").then(response => {
@@ -10,21 +10,75 @@ class GetData{
 }
 
 class Filter{
-    filter(platform){
-        console.log(platform);
+    filteredResult = [];
+    filter(platform, data){
+        for(let i = 0; i < data.length; i++){
+            if(data[i].platform == platform){
+                this.filteredResult.push(data[i]);
+            }
+        }
+
+    }
+
+    randomFromResult(){
+        let randomNumber = Math.floor(Math.random() * this.filteredResult.length);
+        return this.filteredResult[randomNumber];
+    }
+}
+
+class URLScraper{
+    currentURL;
+    platform;
+    pretty;
+    constructor(){
+        this.currentURL = window.location.href;
+    }
+
+    getDataFromURL(){
+        this.platform = this.currentURL.split("platform=")[1];
+        this.pretty = new PrettyPlatform(this.platform);
+        this.platform = this.pretty.platform;
+    }
+}
+
+class PrettyPlatform{
+    platform;
+
+    constructor(platform){
+        this.platform = platform;
+        this.platformToUpperCase();
+        this.removeSpaces();
+    }
+
+    platformToUpperCase(){
+        this.platform = this.platform.toUpperCase()
+    }
+
+    removeSpaces(){
+        this.platform = this.platform.replaceAll(" ", "");
+        this.platform = this.platform.replaceAll("%20", "");
     }
 }
 
 class App{
-    GetData;
+    Api;
     Filter;
+    urlScraper;
 
     constructor(){
-        this.getData = new GetData();
-        this.filter = new Filter()
+        this.api = new Api();
+        this.filter = new Filter();
+        this.urlScraper = new URLScraper();
 
-        this.getData.getData().then(() => {console.log(this.getData)});
-        this.filter.filter("PS4");
+        this.urlScraper.getDataFromURL();
+
+        this.api.getData().then(
+            () => {
+                this.filter.filter(this.urlScraper.platform, this.api.data);
+                let randomResult = this.filter.randomFromResult();
+                console.log(randomResult);
+            }
+            );
     }
 }
 
